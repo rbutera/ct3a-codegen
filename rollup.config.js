6985666cab4ed4760449accb4fcb282212899e59
Rai Butera <rai@rbutera.com>
2024-07-06 00:14:04 +0100

    remove terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "@rollup/plugin-terser";
 
 export default {
   input: "src/index.js",
@@ -16,7 +15,6 @@ export default {
     }),
     commonjs(),
     json(),
-    terser(),
   ],
   external: [
     "plop",
