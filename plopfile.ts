76a459f0eaa400cb0734c18ccada7ae6a7e5fa3a
Rai Butera <rai@rbutera.com>
2024-07-06 10:41:20 +0100

    bump version

--- plopfile.ts
+++ plopfile.ts
@@ -1,7 +1,8 @@
+import { NodePlopAPI } from "plop";
 import { envGenerator } from "./generators/env.js";
 import { routerGenerator } from "./generators/router.js";
 
-export default function (plop) {
+export default function (plop: NodePlopAPI) {
   envGenerator(plop);
   routerGenerator(plop);
 }
